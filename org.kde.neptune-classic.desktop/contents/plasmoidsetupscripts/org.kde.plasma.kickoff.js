811a3a989bbfb087f3ad488edf5b72abc114d8aa
Leszek Lesner <leszek.lesner@web.de>
2019-02-06 16:03:54 +0100

    Remove old junk

--- org.kde.neptune-classic.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++
@@ -1,8 +0,0 @@
-applet.currentConfigGroup = ["Shortcuts"]
-applet.writeConfig("global", "Alt+F1")
-
-applet.currentConfigGroup = ["General"]
-applet.writeConfig("icon", "/usr/share/pixmaps/start-kde-neptune.png")
-applet.writeConfig("favorites", ["chromium.desktop","thunderbird.desktop","org.kde.dolphin.desktop","systemsettings.desktop","libreoffice-startcenter.desktop","gimp.desktop","vlc.desktop","org.kde.konsole.desktop","kde4-apper.desktop"])
-
-applet.reloadConfig();
